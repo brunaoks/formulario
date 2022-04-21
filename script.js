78dc677eef0dd207bfd472f023aebe04715183fa
brunao <sfbruno2002@gmail.com>
2022-04-21 14:17:44 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Preencha com cuidado todos os campos");
+
+function cadastrofim() {
+    alert("Formulário enviado com sucesso! Qualquer atualização entraremos em contato via E-mail. Boa sorte!")
+}
